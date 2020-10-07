a14fd414d988859489cf1ba6fb00df7e1c40f7aa
Samuel Mata <Samuelab2@gmail.com>
2020-10-06 23:40:21 -0400

    created model of the select inputs

---
+++ backend/components/conveyance/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+
+const conveyanceSchema = new Schema({
+  transportation: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  emission_factor: {
+    type: Schema.Types.Decimal128,
+    required: true,
+  }
+})
+
+const Conveyance = model('Conveyance', conveyanceSchema, 'conveyances')
+
+module.exports = Conveyance
