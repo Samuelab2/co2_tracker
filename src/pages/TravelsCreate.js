3b13b994e3d55cef3c270c043f1cbb819d639a1c
Samuel Mata <Samuelab2@gmail.com>
2020-10-07 12:29:15 -0400

    styling create page

--- src/pages/TravelsCreate.js
+++ src/pages/TravelsCreate.js
@@ -59,61 +59,91 @@ const TravelsCreate = () => {
   }
 
   return (
-    <>
-      <h1>Travels Create</h1>
-      <Link to="/travels" className='btn btn-primary'>Volver</Link>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-group">
-          <label htmlFor="date">Fecha</label> <br/>
-          <DatePicker showTimeSelect dateFormat="dd/MM/yyyy" className="form-control" selected={startDate} onChange={date => setStartDate(date)} />
+    <div className='min-vh-100 bg-light'>
+      <nav className="navbar navbar-dark bg-info">
+        <h1 className="navbar-brand">CO2_Foot Tracker</h1>
+        <Link to="/travels" className='btn btn-light'>Volver</Link>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col text-center m-5">
+            <h1>Nuevo viaje</h1>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="start_address">Punto de partida</label>
-          <input name="start_address" type="text" className="form-control" id="start_address" ref={register({ required: true })} />
-          {errors.start_address && <span>This field is required</span>}
+        <div className="row">
+          <div className="col-12">
+            <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column justify-content-center align-items-center">
+              <div className="form-group col-6">
+                <label htmlFor="date">Fecha</label> <br/>
+                <DatePicker showTimeSelect dateFormat="dd/MM/yyyy" className="form-control" selected={startDate} onChange={date => setStartDate(date)} />
+              </div>
+              <div className="form-group col-6">
+                <label htmlFor="start_address">Punto de partida</label>
+                <input name="start_address" type="text" className="form-control" id="start_address" ref={register({ required: true })} />
+                {errors.start_address && <span>Este campo es requerido</span>}
+              </div>
+              <div className="form-group col-6">
+                <label htmlFor="end_address">Punto de termino</label>
+                <input name="end_address" type="text" className="form-control" id="end_address" ref={register({ required: true })} />
+                {errors.end_address && <span>Este campo es requerido</span>}
+              </div>
+              <div className="form-group col-6">
+                <label htmlFor="transportation">Medio de transporte</label>
+                <select onChange={transportationHandleChange} name="transportation" className="form-control" id="transportation" ref={register({ required: true })}>
+                  {
+                    conveyances ?
+                    conveyances.map((conveyance, index) => {
+                      return (
+                        <option key={index} value={index}>{conveyance.transportation}</option>
+                      )
+                    })
+                    : <option>Cargando...</option>
+                  }
+                </select>
+                {errors.transportation && <span>Este campo es requerido</span>}
+              </div>
+              <div className="form-group col-6">
+                <label htmlFor="km">Cantidad de Kilometros</label>
+                <input onChange={kmHandleChange} name="km" type="number" className="form-control" id="km" ref={register({ required: true })} />
+                {errors.km && <span>Este campo es requerido</span>}
+              </div>
+              <div className="form-group col-6">
+                <label htmlFor="number_workers">Cantidad de trabajadores</label>
+                <input name="number_workers" type="number" className="form-control" id="number_workers" ref={register({ required: true })} />
+                {errors.number_workers && <span>Este campo es requerido</span>}
+              </div>
+              <div className="form-group form-check col-5">
+                <input onChange={roundtripHandleChange} name="round_trip" type="checkbox" className="form-check-input" id="round_trip" />
+                <label className="form-check-label" htmlFor="round_trip">Ida y vuelta</label>
+                {errors.round_trip && <span>Este campo es requerido</span>}
+              </div>
+              <button type="submit" className="btn btn-primary">Guardar</button>
+            </form>
+          </div>
+        
+          <div className="col-6 mt-4 mx-auto">
+            <table className="table table-bordered">
+              <thead className="thead-light text-center">
+                <tr>
+                  <th scope="col">[ kgCO2/ km] </th>
+                  <th scope="col">KM del recorrido</th>
+                  <th scope="col">Ida y vuelta</th>
+                  <th scope="col">Total KgCO2</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{inputTransportation.emission_factor}</td>
+                  <td>{inputKM}</td>
+                  <td>{inputRoundTrip}</td>
+                  <td>{totalCoFoot} KgCO2</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="end_address">Punto de termino</label>
-          <input name="end_address" type="text" className="form-control" id="end_address" ref={register({ required: true })} />
-          {errors.end_address && <span>This field is required</span>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="transportation">Medio de transporte</label>
-          <select onChange={transportationHandleChange} name="transportation" className="form-control" id="transportation" ref={register({ required: true })}>
-            {
-              conveyances ?
-              conveyances.map((conveyance, index) => {
-                return (
-                  <option key={index} value={index}>{conveyance.transportation}</option>
-                )
-              })
-              : <option>Cargando...</option>
-            }
-          </select>
-          {errors.transportation && <span>This field is required</span>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="km">Cantidad de Kilometros</label>
-          <input onChange={kmHandleChange} name="km" type="number" className="form-control" id="km" ref={register({ required: true })} />
-          {errors.km && <span>This field is required</span>}
-        </div>
-        <div className="form-group">
-          <label htmlFor="number_workers">Cantidad de trabajadores</label>
-          <input name="number_workers" type="number" className="form-control" id="number_workers" ref={register({ required: true })} />
-          {errors.number_workers && <span>This field is required</span>}
-        </div>
-        <div className="form-group form-check">
-          <input onChange={roundtripHandleChange} name="round_trip" type="checkbox" className="form-check-input" id="round_trip" />
-          <label className="form-check-label" htmlFor="round_trip">Ida y vuelta</label>
-          {errors.round_trip && <span>This field is required</span>}
-        </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
-      <p>{inputTransportation.emission_factor}</p>
-      <p>{inputKM}</p>
-      <p>{inputRoundTrip}</p>
-      <p>{totalCoFoot}</p>
-    </>
+      </div>
+    </div>
   )
 }
 
