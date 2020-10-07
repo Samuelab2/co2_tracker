0881660f9eaacd32ac5671061abdc04f4f163c80
Samuel Mata <Samuelab2@gmail.com>
2020-10-06 23:39:39 -0400

    minor refactor to the connection file

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+const { connection } = mongoose
+require('dotenv').config()
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI
 mongoose.connect(uri, { 
   useNewUrlParser: true, 
   useCreateIndex: true,
   useUnifiedTopology: true,
   }
-);
-const connection = mongoose.connection;
+)
+
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+  console.log("MongoDB database connection established successfully")
 })
