6a0ca76b311d5d62279ead5fa8f5280e22d11af7
Samuel Mata <Samuelab2@gmail.com>
2020-10-07 12:40:20 -0400

    modified meta tag in the html and styled the 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
+    <div className='d-flex justify-content-center align-items-center min-vh-100 bg-light'>
       <h1>Ups... la pagina que estabas buscando no existe <span role='img' aria-label='emoji triste'>😣</span></h1>
+      <Link to='/travels' className='btn btn-primary'>
+        Regresar
+      </Link>
+    </div>
   )
 }
 
